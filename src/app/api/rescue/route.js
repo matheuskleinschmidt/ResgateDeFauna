ec040acca9496a07407507c9a324fa7801c82a3e
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-08-29 15:03:12 -0300

    fix cors

--- src/app/api/rescue/route.js
+++ src/app/api/rescue/route.js
@@ -7,9 +7,24 @@ export async function GET(req) {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
     },
   });
 }
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    },
+  });
+}
+
 
 export async function POST(req) {
   return new Response(JSON.stringify({ text: 'Hello' }), {
