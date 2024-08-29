43078684ea8484c81efb95f93773f6a7d3ca3b26
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-08-28 22:46:20 -0300

    fix menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 import styles from "./page.module.css";
-import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <div>
-      <Button>tets</Button>
+      
     </div>
   );
 }
