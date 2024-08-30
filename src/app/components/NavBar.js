6fe63f364942344ca4ff1c254ba9c9e4767a665f
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-08-30 11:59:28 -0300

    fix pages

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,37 +1,46 @@
 "use client";
 
 import React from "react";
-import {Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
-import {AcmeLogo} from "./AcmeLogo.jsx";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+  NavbarContent,
+  NavbarItem,
+  Link,
+} from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems = [
-    { label: "Resgates", href: "/rescue" },
-  ];
+  const menuItems = [{ label: "Resgates", href: "/rescue" }];
 
   return (
-    <Navbar
-      isBordered
-      isMenuOpen={isMenuOpen}
-      onMenuOpenChange={setIsMenuOpen}
-    >
+    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
       </NavbarContent>
 
       <NavbarContent className="sm:hidden pr-3" justify="center">
         <NavbarBrand>
           <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
+          <Link color="foreground" href="/">
+            <p className="font-bold text-inherit">ACME</p>
+          </Link>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarBrand>
           <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
+          <Link color="foreground" href="/">
+            <p className="font-bold text-inherit">ACME</p>
+          </Link>
         </NavbarBrand>
         <NavbarItem>
           <Link color="foreground" href="/rescue">
@@ -43,14 +52,7 @@ export default function App() {
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item.label}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              href={item.href}
-              size="lg"
-            >
+            <Link className="w-full" href={item.href} size="lg">
               {item.label}
             </Link>
           </NavbarMenuItem>
--- src/app/rescue/page.js
+++ src/app/rescue/page.js
@@ -10,6 +10,10 @@ import {
   TableRow,
   TableCell,
 } from "@nextui-org/table";
+import moment from "moment";
+import "moment-timezone";
+
+const timezone = 'America/Sao_Paulo'; // Timezone do Brasil
 
 const RescuePage = () => {
   const [rescues, setRescues] = useState([]);
@@ -35,25 +39,64 @@ const RescuePage = () => {
   }, []);
 
   return (
-    <div style={{ overflowX: "auto" }}>
+    <div className="responsive-table">
+    
       <Table isStriped="true" isCompact="true">
         <TableHeader>
           <TableColumn>Espécie</TableColumn>
-          <TableColumn>situação</TableColumn>
+          <TableColumn>Situação</TableColumn>
           <TableColumn>Chamado via</TableColumn>
-          <TableColumn>Data</TableColumn>
+          <TableColumn>Data e hora</TableColumn>
         </TableHeader>
         <TableBody items={rescues}>
           {(rescues) => (
             <TableRow key={rescues.id}>
-              {<TableCell>{rescues.species}</TableCell>}
-              {<TableCell>{rescues.situation}</TableCell>}
-              {<TableCell>{rescues.calledVia}</TableCell>}
-              {<TableCell>{rescues.date}</TableCell>}
+              <TableCell>{rescues.species}</TableCell>
+              <TableCell>{rescues.situation}</TableCell>
+              <TableCell>{rescues.calledVia}</TableCell>
+              <TableCell>{moment(rescues.date).tz(timezone).format('DD/MM/YYYY HH:mm:ss')}</TableCell>
             </TableRow>
           )}
         </TableBody>
       </Table>
+
+      <style jsx>{`
+        .responsive-table {
+          overflow-x: auto;
+        }
+
+        @media (max-width: 768px) {
+          .responsive-table {
+            display: block;
+            width: 100%;
+          }
+          table {
+            width: 100%;
+            table-layout: auto;
+          }
+          thead {
+            display: none;
+          }
+          tbody tr {
+            display: block;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+          }
+          tbody td {
+            display: flex;
+            justify-content: space-between;
+            padding: 10px;
+            border-bottom: 1px solid #ddd;
+          }
+          tbody td:before {
+            content: attr(data-label);
+            flex: 1;
+            text-align: left;
+            font-weight: bold;
+            padding-right: 10px;
+          }
+        }
+      `}</style>
     </div>
   );
 };
