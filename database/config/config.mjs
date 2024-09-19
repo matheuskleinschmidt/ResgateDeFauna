7583a69a88b9b41c20a72fb53825f49d980a1efa
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-09-18 22:28:31 -0300

    Update config.mjs

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -6,7 +6,7 @@ export const options = {
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   dialect: "postgres",
-  Logging: process.env.NODE_ENV === "development" ? console.log : false,
+  Logging:  console.log,
   migrationStorageTableName: "migrations",
 };
 
