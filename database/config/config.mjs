1d0f5fdbbb12a8ddc44dd978f4c68ddf87243b2b
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-09-18 22:46:58 -0300

    test

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -6,6 +6,12 @@ export const options = {
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   dialect: "postgres",
+  "dialectOptions": {
+    "ssl": {
+      "require": true,
+      "rejectUnauthorized": false
+    }
+  },
   Logging: process.env.NODE_ENV === "development" ? console.log : false,
   migrationStorageTableName: "migrations",
 };
