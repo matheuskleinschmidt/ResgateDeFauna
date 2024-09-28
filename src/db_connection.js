3ff9906b3116ef5506a488a74a0aa40968737575
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-09-28 16:29:24 -0300

    fix pg..

--- src/db_connection.js
+++ src/db_connection.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 import { options } from "../database/config/config.mjs";
+import pg from 'pg';
 
-const sequelize = new Sequelize(options) 
+const sequelize = new Sequelize({ ...options, dialectModule: pg }) 
 
 export default sequelize;
