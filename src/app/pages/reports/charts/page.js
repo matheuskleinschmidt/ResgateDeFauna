3e12ea6b52f9b1dba0d9986fac30d4d87d611c3c
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-10-25 17:21:25 +0000

    adicionando exemplo

--- src/app/pages/reports/charts/page.js
+++ src/app/pages/reports/charts/page.js
@@ -1,10 +1,62 @@
-import React from "react";
-import { Card, CardBody, Link } from "@nextui-org/react";
+"use client";
 
-export default function configurationsPage() {
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  hartTooltip,
+  ChartTooltipContent,
+  ChartTooltip,
+} from "@/components/ui/chart";
+
+import MonthRescues from "@/app/components/MonthRescues"
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+};
+
+export function Component() {
+  return (
+    <ChartContainer config={chartConfig}>
+      <BarChart accessibilityLayer data={chartData}>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <ChartTooltip content={<ChartTooltipContent />} />
+        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+      </BarChart>
+    </ChartContainer>
+  );
+}
+
+export default function Chart() {
   return (
     <div>
       <div>
+        <MonthRescues />
       </div>
     </div>
   );
