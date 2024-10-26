2aa383ce83653974e39ab3056eed2623f23966a3
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-10-26 02:27:58 +0000

    add url cache

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const url = require('url');
+const path = require("path");
+const url = require("url");
 
 /** @type {import('next').NextConfig} */
 
@@ -14,15 +14,40 @@ const withPWA = require("@ducanh2912/next-pwa").default({
     disableDevLogs: true,
     runtimeCaching: [
       {
-        urlPattern: ({ url }) => url.pathname.startsWith('/api/'),
-        handler: 'NetworkOnly',
-        method: 'POST',
+        urlPattern: ({ url }) => url.pathname.startsWith("/api/"),
+        handler: "NetworkOnly",
+        method: "POST",
         options: {
           backgroundSync: {
-            name: 'apiQueue',
+            name: "apiQueue",
             options: {
               maxRetentionTime: 24 * 60,
             },
+          },
+        },
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "offlineCache",
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+      {
+        urlPattern: /\/api\/.*$/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "apiCache",
+          networkTimeoutSeconds: 10,
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 86400,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
           },
         },
       },
@@ -30,7 +55,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   },
 });
 
-const dirname = __dirname; 
+const dirname = __dirname;
 
 const nextConfig = {
   experimental: {
@@ -38,7 +63,12 @@ const nextConfig = {
     esmExternals: true,
   },
   webpack: (config) => {
-    config.resolve.alias['rlayers'] = path.resolve(dirname, 'node_modules', 'rlayers', 'dist');
+    config.resolve.alias["rlayers"] = path.resolve(
+      dirname,
+      "node_modules",
+      "rlayers",
+      "dist"
+    );
     return config;
   },
 };
