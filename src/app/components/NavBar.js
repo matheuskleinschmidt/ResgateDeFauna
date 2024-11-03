fc76e9267d48a5674069afc06e254eaec6376518
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-11-03 23:37:52 +0000

    fix navbar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -100,64 +100,53 @@ export default function App() {
 
   return (
     <Navbar
-      isBordered
-      isMenuOpen={isMenuOpen}
-      onMenuOpenChange={setIsMenuOpen}
-      className="mb-4"
+    isBordered
+    isMenuOpen={isMenuOpen}
+    onMenuOpenChange={setIsMenuOpen}
+    className="mb-4"
+  >
+    <NavbarContent
+      justify="start"
+      align="center"
+      css={{ flex: 1 }}
     >
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-        />
-      </NavbarContent>
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Fechar menu" : "Abrir menu"}
+      />
+    </NavbarContent>
 
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand>
-          <AcmeLogo />
-          <Link color="foreground" href="/">
-            <p className="font-bold text-inherit">Fujama</p>
-          </Link>
-        </NavbarBrand>
-      </NavbarContent>
+    <NavbarContent
+      justify="end"
+      align="center"
+      css={{ flex: 1 }}
+    >
+      <NavbarBrand>
+        <AcmeLogo />
+        <Link color="foreground" href="/">
+          <p className="font-bold text-inherit">Fujama</p>
+        </Link>
+      </NavbarBrand>
+    </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-          <AcmeLogo />
-          <Link color="foreground" href="/">
-            <p className="font-bold text-inherit">Fujama</p>
-          </Link>
-        </NavbarBrand>
-        <NavbarItem>
-          <Link color="foreground" href="/pages/rescue">
-            Resgates
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/pages/rescue/addRescue">
-            Adicionar Resgate
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item.label}-${index}`}>
-            <Link className="w-full" href={item.href} size="lg">
-              {item.label}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-        <NavbarMenuItem>
-          <Link className="w-full" size="lg">
-            <button onClick={handleRefresh}>Renovar cache</button>
+    <NavbarMenu>
+      {menuItems.map((item, index) => (
+        <NavbarMenuItem key={`${item.label}-${index}`}>
+          <Link className="w-full" href={item.href} size="lg">
+            {item.label}
           </Link>
         </NavbarMenuItem>
-        <NavbarMenuItem>
-          <Link className="w-full" size="lg">
-            <button onClick={() => signOut()}>Sair</button>
-          </Link>
-        </NavbarMenuItem>
-      </NavbarMenu>
-    </Navbar>
+      ))}
+      <NavbarMenuItem>
+        <Link className="w-full" size="lg">
+          <button onClick={handleRefresh}>Renovar cache</button>
+        </Link>
+      </NavbarMenuItem>
+      <NavbarMenuItem>
+        <Link className="w-full" size="lg">
+          <button onClick={() => signOut()}>Sair</button>
+        </Link>
+      </NavbarMenuItem>
+    </NavbarMenu>
+  </Navbar>
   );
 }
