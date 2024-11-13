91e929460ff0714fdbce79fb6adbf5168ee1bc60
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-11-13 16:35:25 +0000

    dix coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import path from 'path';
- 
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    exclude: ['**/*.controller.test.js','**/node_modules/**',],
-    setupFiles: './setupTests.js',
+    environment: "jsdom",
+    exclude: ["**/*.controller.test.js", "**/node_modules/**"],
+    setupFiles: "./setupTests.js",
+    coverage: { exclude: ['database/**','**/route.js', '*.config.js','**/.next/**', '**/cypress/**', '**/*.controller.test.js','**/api/**', '**/public/**'] },
   },
-})
+});
