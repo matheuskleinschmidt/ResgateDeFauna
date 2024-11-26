e2f1e375b879d453e0a830397e2ecb2234d8024f
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-11-26 22:51:54 +0000

    fix test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -14,6 +14,6 @@ export default defineConfig({
     environment: "jsdom",
     exclude: ["**/*.controller.test.js", "**/node_modules/**"],
     setupFiles: "./setupTests.js",
-    coverage: { exclude: ['database/**','**/route.js', '*.config.js','**/.next/**', '**/cypress/**', '**/*.controller.test.js','**/api/**', '**/public/**'] },
+    coverage: { exclude: ['database/**','**/route.js', '*.config.js','**/.next/**', '**/cypress/**', '**/*.controller.test.js','**/api/**', '**/public/**', '**/components/ui/**'] },
   },
 });
