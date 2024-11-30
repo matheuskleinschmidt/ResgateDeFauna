dd3ddd288f02ad378441224083724eb3c572d6b0
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2024-11-30 04:52:46 +0000

    fix home page UI

--- src/app/__tests__/HomeList.component.test.jsx
+++ src/app/__tests__/HomeList.component.test.jsx
@@ -17,12 +17,13 @@ describe('App Component', () => {
     render(<App />);
 
     const cards = screen.getAllByTestId('card');
-    expect(cards).toHaveLength(4);
+    expect(cards).toHaveLength(5);
 
     const expectedLinks = [
       { text: 'Adicionar resgate', href: '/pages/rescue/addRescue' },
       { text: 'Listar resgates', href: '/pages/rescue' },
-      { text: 'Relatórios', href: '/pages/reports' },
+      { text: 'Mapas', href: '/pages/reports/maps' },
+      { text: 'Gráficos', href: '/pages/reports/charts' },
       { text: 'Configurações', href: '/pages/configurations' },
     ];
 
@@ -37,7 +38,7 @@ describe('App Component', () => {
     render(<App />);
 
     const links = screen.getAllByTestId('link');
-    expect(links).toHaveLength(4);
+    expect(links).toHaveLength(5);
 
     const expectedLinks = [
       { text: 'Adicionar resgate', href: '/pages/rescue/addRescue' },
@@ -57,7 +58,7 @@ describe('App Component', () => {
     render(<App />);
 
     const cards = screen.getAllByTestId('card');
-    expect(cards).toHaveLength(4);
+    expect(cards).toHaveLength(5);
 
     cards.forEach(card => {
       expect(card).toHaveClass('w-full', 'max-w-xs', 'mb-4');
--- src/app/components/HomeList.jsx
+++ src/app/components/HomeList.jsx
@@ -4,26 +4,41 @@ import { Card, CardBody, Link } from "@nextui-org/react";
 export default function App() {
   return (
     <div className="flex flex-col items-center justify-center max-w-full px-4 mx-auto sm:max-w-md">
-      <Card  className="w-full max-w-xs mb-4">
+      <Card className="w-full max-w-xs mb-4">
         <CardBody>
-        <Link href="/pages/rescue/addRescue"><p>Adicionar resgate</p></Link>
+          <Link href="/pages/rescue/addRescue">
+            <p>Adicionar resgate</p>
+          </Link>
         </CardBody>
       </Card>
-      <Card  className="w-full max-w-xs mb-4">
+      <Card className="w-full max-w-xs mb-4">
         <CardBody>
-        <Link href="/pages/rescue"><p>Listar resgates</p></Link>
+          <Link href="/pages/rescue">
+            <p>Listar resgates</p>
+          </Link>
         </CardBody>
       </Card>
-      <Card  className="w-full max-w-xs mb-4">
+      <Card className="w-full max-w-xs mb-4">
         <CardBody>
-        <Link href="/pages/reports"><p>Relatórios</p></Link>
+          <Link href="/pages/reports/maps">
+            <p>Mapas</p>
+          </Link>
         </CardBody>
       </Card>
-      <Card  className="w-full max-w-xs mb-4">
+      <Card className="w-full max-w-xs mb-4">
         <CardBody>
-        <Link href="/pages/configurations"><p>Configurações</p></Link>
+          <Link href="/pages/reports/charts">
+            <p>Gráficos</p>
+          </Link>
+        </CardBody>
+      </Card>
+      <Card className="w-full max-w-xs mb-4">
+        <CardBody>
+          <Link href="/pages/configurations">
+            <p>Configurações</p>
+          </Link>
         </CardBody>
       </Card>
     </div>
-    );
+  );
 }
